89cb17fbc205dbfeed154f66cc09f889c036e5e8
Eszter <szteran@gmail.com>
2023-10-18 11:31:43 +0200

    albumTitlegen.

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ let adjectives = ["smelly", "hairy", "fat", "magical", "popular", "little", "awe
 
 let conjunctions = ["/", "-",  "#",  "&", "and",  "at", "of", "on", "in"]
 
-let adverb = ["tonight", "at dawn", "here", "angrily", "crying", ]
+let adverbs = ["tonight", "at dawn", "here", "angrily", "crying", ]
 
-let verb = ["rest", "sleep", "swig", "faint", "throw up", "somersault", ]
+let verbs = ["rest", "sleep", "swig", "faint", "throw up", "somersault", ]
+
+
 
 
 function bandNameGenerator(string){  //awesome 6                  //The returned band name shouldn't contain the string given as an argument
@@ -28,40 +30,48 @@ function bandNameGenerator(string){  //awesome 6                  //The returned
 
 
 
-
-
  //Album title generator: The (nouns) (verbs) (adverbs)
 
+function albumTitleGenerator(bandName) {
+  let secondWord = ""
+  let thirdWord = "";
+  let fourthWord = "";
 
+  //Second word   - "the hhhkkl kiskacsa" = 19 char
+  for(let i = 0; i < bandName.length; i++) {
+    if((nouns.length >= bandName.length) && (bandName[i] === "a")) {
+        secondWord = nouns[i]
+    } else if ((nouns.length >= bandName.length) && (bandName[i] === "i")) {
+        secondWord = nouns[i]
+    } else {
+      secondWord = nouns[bandName.length * 7 % 9]
+    }  
+  }
 
-function albumTitleGenerator(bandNameGenerator) {
-    let secondWord = ""
-    let thirdWord = "";
-    let fourthWord = "";
+//Third word
+  let counter = 0;
+    for(let i = 0; i < bandName.length; i++) {  
+        if ((bandName[i] === "a") || (bandName[i] === "i") || (bandName[i] === "o")) {
+          counter += i
+        }
+  }
+  thirdWord = verbs[(counter * bandName.length) % verbs.length]
 
-    for(let i = 0; i < bandNameGenerator.length; i++)
-    if(bandNameGenerator.length <= 10) {
-        if(bandNameGenerator[i] === "a") {
-            if (nouns.length < i) {
-                secondWord = nouns[i];
-            } else {
-                secondWord = nouns[nouns.length - i % 7]
-            }
-        
-          {
-             
-        } 
-
-
+//Fourt word
+  let counter2 = 0;
+    for(let i = 0; i < bandName.length; i++) {
+      counter2 += bandName.charCodeAt(i); 
     }
-    }
+  fourthWord = adverbs[counter2 % adverbs.length]  
 
-    return `The ${secondWord} ${thirdWord} ${fourthWord}` 
+  return `The ${secondWord} ${thirdWord} ${fourthWord}` 
+
 }
-
-let string = "nemmegyaz";
-console.log(bandNameGenerator(string))
-//console.log(albumTitleGenerator)
-
-
+    
+console.log(albumTitleGenerator("the Wolves hairy"))
+console.log(albumTitleGenerator("the Mice magical"))
+console.log(albumTitleGenerator("the Lizards dirty"))
+console.log(albumTitleGenerator("the Moonwalkers fat"))
+console.log(albumTitleGenerator("the hsjdll glee"))
+console.log(albumTitleGenerator("the Marathon Runners popular"))
 
